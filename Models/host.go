c0f7c690d8112516fd8dc8c48e5757ce0c58d18d
Daniela Cortes Antonio <dcortesantonio@gmail.com>
2020-06-12 00:00:51 -0500

    Documentation of Models

--- Models/host.go
+++ Models/host.go
@@ -1,27 +1,126 @@
 package Models
 
+//Endpoint
+//
+// This represents an endpoint of a domain searched, given in a HTTP Service.
+//
+// swagger:model
 type Endpoint struct {
+	// the IP address of the endpoint.
+	//
+	// required: true
+	// example: 172.217.0.46
 	IpAddress         string `json:"ipAddress"`
+	// the name of the server searched.
+	//
+	// required: true
+	// example: lga15s43-in-f46.1e100.net
 	Server_Name       string `json:"serverName"`
+	// the status of the endpoint searched.
+	//
+	// required: true
+	// example: READY
 	Status_Message    string `json:"statusMessage"`
+	// the SSL grade qualification of this endpoint.
+	//
+	// required: true
+	// example: B
 	SSL_Grade         string `json:"grade"`
+	// the SSL grade trust of this endpoint.
+	//
+	// required: true
+	// example: B
 	GradeTrustIgnored string `json:"gradeTrustIgnored"`
+	// if this endpoint have warnings.
+	//
+	// required: true
+	// example: false
 	Has_Warnings      bool   `json:"hasWarnings"`
+	// if the server is exceptional.
+	//
+	// required: true
+	// example: false
 	Is_Exceptional    bool   `json:"isExceptional"`
+	// the progress of this endpoint.
+	//
+	// required: true
+	// example: 100
 	Progress          int64  `json:"progress"`
+	// the duration of this endpoint.
+	//
+	// required: true
+	// example: 100569
 	Duration          int64  `json:"duration"`
+	// the delegation of this endpoint.
+	//
+	// required: true
+	// example: 1
 	Delegation        int8   `json:"delegation"`
 }
 
+//Host
+//
+// This represents the SSL and servers information of a domain searched, given in a HTTP Service.
+//
+// swagger:model
 type Host struct {
+	// the domain searched
+	//
+	// required: true
+	// example: google.com
 	Host             string     `json:"host"`
+	// the port of the domain searched
+	//
+	// required: true
+	// example: 443
 	Port             int16      `json:"port"`
+	// the protocol that use the domain searched
+	//
+	// required: true
+	// example: http
 	Protocol         string     `json:"protocol"`
+	// if the the domain searched is public
+	//
+	// required: true
+	// example: false
 	Is_Public        bool       `json:"isPublic"`
+	// the port of the domain searched
+	//
+	// required: true
+	// example: 443
 	Status           string     `json:"status"`
+	// the status of the domain searched
+	//
+	// required: true
+	// example: 1591917234433
 	StartTime        int64      `json:"startTime"`
+	// the test time of the domain searched
+	//
+	// required: true
+	// example: 1591917435246
 	TestTime         int64      `json:"testTime"`
+	// the engine version of the domain searched
+	//
+	// required: true
+	// example: 2.1.5
 	Engine_Version   string     `json:"engineVersion"`
+	// the criteria version of the domain searched
+	//
+	// required: true
+	// example: 2009q
 	Criteria_Version string     `json:"criteriaVersion"`
+	// the endpoints version of the domain searched
+	//
+	// required: true
+	// Extensions:
+	// ---
+	// x-property-value: value
+	// x-property-array:
+	//   - value1
+	//   - value2
+	// x-property-array-obj:
+	//   - name: obj
+	//     value: field
+	// ---
 	Endpoints        []Endpoint `json:"endpoints"`
 }
--- Models/ipInfo.go
+++ Models/ipInfo.go
@@ -1,18 +1,79 @@
 package Models
 
+//IPinfo
+//
+// This represents the location information of an IP, given in a HTTP Service.
+//
+// swagger:model
 type IPInfo struct {
+	// the IP address of the endpoint.
+	//
+	// required: true
+	// example: 172.217.0.46
 	Query       string  `json:"query"`
+	// the status of the endpoint.
+	//
+	// required: true
+	// example: success
 	Status      string  `json:"status"`
+	// the country where is the endpoint server.
+	//
+	// required: true
+	// example: United States
 	Country     string  `json:"country"`
+	// the code of the country where is the endpoint server.
+	//
+	// required: true
+	// example: US
 	CountryCode string  `json:"countryCode"`
+	// the region where is the endpoint server.
+	//
+	// required: true
+	// example: NY
 	Region      string  `json:"region"`
+	// the name of the region where is the endpoint server.
+	//
+	// required: true
+	// example: New York
 	RegionName  string  `json:"regionName"`
+	// the city where is the endpoint server.
+	//
+	// required: true
+	// example: New York
 	City        string  `json:"city"`
+	// the zip of the endpoint server.
+	//
+	// required: true
+	// example: 10123
 	Zip         string  `json:"zip"`
+	// the lat of the endpoint server.
+	//
+	// required: true
+	// example: 40.7128
 	Lat         float64 `json:"lat"`
+	// the lon of the endpoint server.
+	//
+	// required: true
+	// example: 74.006
 	Lon         float64 `json:"lon"`
+	// the TimeZone where is the endpoint server.
+	//
+	// required: true
+	// example: America/New_York
 	Timezone    string  `json:"timezone"`
+	// the isp of the endpoint server.
+	//
+	// required: true
+	// example: Google LLC
 	Isp         string  `json:"isp"`
+	// the org of the endpoint server.
+	//
+	// required: true
+	// example: Google LLC
 	Org         string  `json:"org"`
+	// the as of the endpoint server.
+	//
+	// required: true
+	// example: AS15169 Google LLC
 	As          string  `json:"as"`
 }
